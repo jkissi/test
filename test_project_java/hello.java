f868f41d57c699a379395289a76c6e389461dcc0
jon <kissiameyaw@gmail.com>
2014-10-28 10:44:50 -0400

    Adding java file to repository.

---
+++ test_project_java/hello.java
@@ -0,0 +1,11 @@
+/*
+ * A short program to test if the java compiler is working correctly
+ * */
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!!!");
+	}
+}
